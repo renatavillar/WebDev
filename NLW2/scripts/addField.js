be84d1b71851a73ee7bda40cd6e9cd06539f2033
renatavillar <60142571+renatavillar@users.noreply.github.com>
2020-08-06 21:08:57 -0300

    Update addField.js

--- NLW2/scripts/addField.js
+++ NLW2/scripts/addField.js
@@ -1,1 +1,18 @@
+// Procurar o botao
+document.querySelector("#add-time")
+// Quando clicar no botao
+.addEventListener('click', cloneField)
+// Executar uma acao
+function cloneField() {
+	// Duplicar os campos. Que campos?
+	const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+	// limpar os campos. Que campos?
+	const fields = newFieldContainer.querySelectorAll('input')
 
+	fields.forEach(function(field) {
+		// pega o field do momento e limpa ele
+		field.value = ""
+	})
+	// Colocar na pagina. Onde?
+	document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
